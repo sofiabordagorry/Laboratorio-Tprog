09fbbb70c81fc5b4b9f3dddd3180fd05bdadcff5
Joaquin Corbo Pereira <joaquin.corbo@fing.edu.uy>
2023-08-25 17:22:27 -0300

    PaqueteTipo

---
+++ Tarea1/src/logica/PaqueteTipo.java
@@ -0,0 +1,19 @@
+package logica;
+
+public class PaqueteTipo {
+	private int cantidad;
+	private Tipo tipo;
+	
+	public PaqueteTipo(int cantidad, Tipo tipo) {
+		this.cantidad = cantidad;
+		this.tipo = tipo;
+	}
+	
+	public int getCantidad() {
+		return this.cantidad;
+	}
+	
+	public Tipo getTipo() {
+		return this.tipo;
+	}
+}
