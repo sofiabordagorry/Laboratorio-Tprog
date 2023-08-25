e4c631af34a9620ed50a5be452fafa0120a9dbfe
Sofía Belén Bordagorry Fernández <sofia.bordagorry@fing.edu.uy>
2023-08-25 17:21:14 -0300

    Update DTOfertaLaboral.java

--- Tarea1/src/logica/DTOfertaLaboral.java
+++ Tarea1/src/logica/DTOfertaLaboral.java
@@ -6,17 +6,22 @@ import java.util.Map;
 public class DTOfertaLaboral {
 	
 	private String nombre;
+	private String descripcion;
 	private String ciudad;
 	private String departamento;
-	private int horario;
+	private String horario;
 	private float remuneracion;
 	private LocalDate fechaDeAlta;
 	private float costoAsociado;
 	private DTTipo dataTipo;
 	private Map<String, DTKeyword> dataKeywords;
+	private List<DTPostulacion> dataPostulaciones;
+	private DTEmpresa dataEmpresa;
+	private DTCompra dataCompra;
 	
-	public DTOfertaLaboral(String nombre, String ciudad, String departamento, int horario, float remuneracion, LocalDate fechaDeAlta, float costoAsociado, DTTipo dataTipo, Map<String, DTKeyword> dataKeywords) {
+	public DTOfertaLaboral(String nombre, String descripcion, String ciudad, String departamento, String horario, float remuneracion, LocalDate fechaDeAlta, float costoAsociado, DTTipo dataTipo, Map<String, DTKeyword> dataKeywords, List<DTPostulacion> dataPostulaciones, DTEmpresa dataEmpresa, DTCompra dataCompra) {
 		this.nombre = nombre;
+		this.descripcion = descripcion;
 		this.ciudad = ciudad;
 		this.departamento = departamento;
 		this.horario = horario;
@@ -25,10 +30,29 @@ public class DTOfertaLaboral {
 		this.costoAsociado = costoAsociado;
 		this.dataTipo = dataTipo;
 		this.dataKeywords = dataKeywords;
+		this.dataPostulaciones = dataPostulaciones;
+		this.dataEmpresa = dataEmpresa;
+		this.dataCompra = dataCompra;
+	}
+	
+	public DTOfertaLaboral(String nombre, String descripcion, String ciudad, String departamento, String horario, float remuneracion) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.ciudad = ciudad;
+		this.departamento = departamento;
+		this.horario = horario;
+		this.remuneracion = remuneracion;
+		this.fechaDeAlta = LocalDate.now();
+		this.dataTipo = null;
+		this.dataKeywords = null;
 	}
 	
 	public String getNombre() {
 		return this.nombre;
+	}
+	
+	public String getDescripcion() {
+		return this.descripcion;
 	}
 	
 	public String getCiudad() {
@@ -39,7 +63,7 @@ public class DTOfertaLaboral {
 		return this.departamento;
 	}
 	
-	public int getHorario() {
+	public String getHorario() {
 		return this.horario;
 	}
 	
@@ -61,5 +85,16 @@ public class DTOfertaLaboral {
 	
 	public Map<String, DTKeyword> getKeywords() {
 		return this.dataKeywords;
+	}
+	
+	public List<DTPostulacion> getPostulaciones(){
+		return this.dataPostulaciones;
+	}
+	
+	public DTEmpresa getDTEmpresa() {
+		return this.dataEmpresa;
+	}
+	public DTCompra getDTCompra() {
+		return this.dataCompra();
 	}
 }
