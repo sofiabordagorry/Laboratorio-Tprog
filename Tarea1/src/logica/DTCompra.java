6178568ce3b66f5e0c79c49392e990723bce5df5
Juan Manuel Pacheco Garcia <juan.pacheco@fing.edu.uy>
2023-08-25 17:26:07 -0300

    Replace DTCompra

--- Tarea1/src/logica/DTCompra.java
+++ Tarea1/src/logica/DTCompra.java
@@ -1,5 +1,38 @@
 package logica;
 
-public class DTCompra {
+import java.time.LocalDate;
+import java.util.Map;
 
+public class DTCompra {
+	private LocalDate fechaCompra;
+	private LocalDate fechaVencimiento;
+	private DTPaquete paqComprado;
+	private Map<String, DTOfertaLaboral> ofertasLaborales;
+	
+	public DTCompra() {
+		fechaCompra = null;
+	}
+	
+	public DTCompra(LocalDate fComp, LocalDate fVenc, DTPaquete paq, Map<String, DTOfertaLaboral> ofLab) {
+		fechaCompra = fComp;
+		fechaVencimiento = fVenc;
+		paqComprado = paq;
+		ofertasLaborales = ofLab;
+	}
+	
+	public LocalDate getFechaCompra() {
+		return fechaCompra;
+	}
+	
+	public LocalDate getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+	
+	public DTPaquete getPaqueteComprado() {
+		return paqComprado;
+	}
+	
+	public Map<String, DTOfertaLaboral> getOfertasLaboralesCompradas(){
+		return ofertasLaborales;
+	}
 }
