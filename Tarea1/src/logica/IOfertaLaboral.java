4c12d056249236f6e008b1dbad39aa48cc213990
Romina Paola Busiello Chagas <romina.busiello@fing.edu.uy>
2023-08-26 11:53:35 -0300

    Update IOfertaLaboral.java

--- Tarea1/src/logica/IOfertaLaboral.java
+++ Tarea1/src/logica/IOfertaLaboral.java
@@ -1,5 +1,20 @@
 package logica;
 
+import java.util.Date;
+import excepciones.TipoRepetidoException;
+import excepciones.NoHayTiposException;
+import excepciones.NoHayPaquetesException;
+import excepciones.TipoYaAgragadoException;
+
 public interface IOfertaLaboral {
 
+	public abstract void ingresarTipo(String nombre, String descripcion, int exposicionT, int duracion, Float costo, Date fechaAlta) throws TipoRepetidoException;
+	
+	public abstract void ingresarKeyword(String nombre);
+	
+	public abstract String[] listarNomPaquetes() throws NoHayPaquetesException;
+	
+	public abstract String[] listarNomTipos() throws NoHayTiposException;
+	
+	public abstract void agregarTipoAPaquete(int cantidad, String tipPaquete, String tipTipo) throws TipoYaAgragadoException;
 }
