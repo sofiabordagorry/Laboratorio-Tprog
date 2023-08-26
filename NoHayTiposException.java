765a0e5c25b627019dae21d2dcd2939186765636
Romina Paola Busiello Chagas <romina.busiello@fing.edu.uy>
2023-08-26 12:03:19 -0300

    Delete NoHayTiposException.java

--- NoHayTiposException.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class NoHayTiposException extends Exception {
-
-    public NoHayTiposException(String string) {
-        super(string);
-    }
-}
