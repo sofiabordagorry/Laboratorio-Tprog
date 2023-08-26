3a3f168f4586859316644730bb7508bcf453847e
Romina Paola Busiello Chagas <romina.busiello@fing.edu.uy>
2023-08-26 12:04:26 -0300

    Add NoHayTiposException

---
+++ Tarea1/src/excepciones/NoHayTiposException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+@SuppressWarnings("serial")
+public class NoHayTiposException extends Exception {
+
+    public NoHayTiposException(String string) {
+        super(string);
+    }
+}
