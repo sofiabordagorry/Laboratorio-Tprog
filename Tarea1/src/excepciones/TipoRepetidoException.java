fd40164c250d4ca77a350a7a52ced5d45e053a1e
Romina Paola Busiello Chagas <romina.busiello@fing.edu.uy>
2023-08-26 12:05:31 -0300

    Add TipoRepetidoException

---
+++ Tarea1/src/excepciones/TipoRepetidoException.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+@SuppressWarnings("serial")
+public class TipoRepetidoException extends Exception {
+
+    public TipoRepetidoException(String string) {
+        super(string);
+    }
+}
