3b56a766798f484220c970009df0e1a40cfcdee4
Joaquin Corbo Pereira <joaquin.corbo@fing.edu.uy>
2023-09-25 19:18:39 -0300

    Update 50 files
    - /Tarea2.1/src/main/webapp/META-INF/MANIFEST.MF
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/EmpresaSinOfertasException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/EmpresasNoExistenException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/ExisteUnUsuarioYaRegistradoException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/KeywordsNoExistenException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/NoExistenEmpresasOfertasLaboralesException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/NoExistenPaquetesException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/NoHayPaquetesException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/NoHayTiposException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/OfertaLaboralRepetidaException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/OfertasLaboralesNoExistenException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/PaqueteRepetidoException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/PostulantesNoExistenException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/TipoPubNoExistenException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/TipoRepetidoException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/TipoYaAgragadoException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/UsuarioNoExisteException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/UsuariosNoExistenException.java
    - /Tarea2.1/src/main/java/com/trabajouy/exceptions/YaSePostuloException.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Compra.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/ControladorOfertaLaboral.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/ControladorUsuario.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTCompra.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTEmpresa.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTKeyword.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTOfertaLaboral.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTPaquete.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTPaqueteTipo.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTPostulacion.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTPostulante.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTTipo.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/DTUsuario.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Empresa.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/EstadoOL.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Factory.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/IOfertaLaboral.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/IUsuario.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Keyword.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/ManejadorOfertaLaboral.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/ManejadorTipo.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/ManejadorUsuario.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/OfertaLaboral.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Paquete.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/PaqueteTipo.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Postulacion.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Postulante.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Tipo.java
    - /Tarea2.1/src/main/java/com/trabajouy/model/Usuario.java
    - /Tarea2.1/.classpath
    - /Tarea2.1/.project

--- Tarea2.1/src/main/java/com/trabajouy/exceptions/EmpresasNoExistenException.java
+++
@@ -1,8 +0,0 @@
-package com.trabajouy.exceptions;
-
-@SuppressWarnings("serial")
-public class EmpresasNoExistenException extends Exception{
-	public EmpresasNoExistenException(String string) {
-		super(string);
-	}
-}
