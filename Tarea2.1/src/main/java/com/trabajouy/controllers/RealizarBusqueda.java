e6d13a8cd4369a30f30c31ba656eb1ea784fa853
joaquin.corbo <joaquin.corbo@fing.edu.uy>
2023-11-12 19:14:57 -0500

    le gustan los rombitos de miercoles

--- Tarea2.1/src/main/java/com/trabajouy/controllers/RealizarBusqueda.java
+++ Tarea2.1/src/main/java/com/trabajouy/controllers/RealizarBusqueda.java
@@ -5,7 +5,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import logica.DTUsuario;
 import publicar.DtEmpresa;
 import publicar.DtOfertaLaboral;
 import publicar.DtOfertaLaboralWS;
